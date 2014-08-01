3c14f6abd81ac2e370651176b4efe8365a9614b5
杨博 <pop.atry@gmail.com>
2014-08-01 16:23:33 +0800

    增加版权信息

--- src/haxe/com/qifun/qforce/importCsv/CsvParser.hx
+++ src/haxe/com/qifun/qforce/importCsv/CsvParser.hx
@@ -1,3 +1,22 @@
+/*
+ * haxe-import-csv
+ * Copyright 2014 深圳岂凡网络有限公司 (Shenzhen QiFun Network Corp., LTD)
+ *
+ * Author: 杨博 (Yang Bo) <pop.atry@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.qifun.qforce.importCsv;
 import haxe.io.BytesOutput;
 import haxe.io.Eof;
@@ -357,3 +376,4 @@ private class InputSource implements ISource
   public var position(get, never):Int;
 
 }
+
